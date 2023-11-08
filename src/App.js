27d6826b5322702a3760378390c3dc672ae364ce
BhuwanArya <aryabhuwan873@gmail.com>
2023-11-08 12:30:34 +0530

    Add:Card qtify-album

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import HeroImage from "./Components/HeroImage/HeroImage";
 import Navbar from "./Components/Navbarcomponent/Navbar"; 
+import Card from "./Components/Cardcomponent/Card";
 
 function App() {
   return (
     <div>
       <Navbar />
       <HeroImage />
+      <Card />
     </div>
   );
 }
